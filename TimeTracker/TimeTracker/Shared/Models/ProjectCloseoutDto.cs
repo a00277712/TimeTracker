4a5317ec63ba99c5e6893418adf2828fd7a12b31
StewartLocke <StewartLocke@MAPS025>
2021-03-18 11:52:01 +0000

    Project Closeouts required fields

--- TimeTracker/TimeTracker/Shared/Models/ProjectCloseoutDto.cs
+++ TimeTracker/TimeTracker/Shared/Models/ProjectCloseoutDto.cs
@@ -1,8 +1,12 @@
-﻿namespace TimeTracker.Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTracker.Shared.Models
 {
     public class ProjectCloseoutDto
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Project Id missing")]
         public int ProjectId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Closeout Reason Required")]
         public int CloseoutReasonId { get; set; }
         public bool CommercialMilestonesComplete { get; set; }
         public bool DeliverablesComplete { get; set; }
@@ -11,10 +15,15 @@
         public string OpsLearnings { get; set; }
         public string LearningActions { get; set; }
         public string FollowUpActions { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Commercial Score Required")]
         public int CommercialScoreId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Operational Score Required")]
         public int OperationalScoreId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Business Development Score Required")]
         public int BusinessDevelopmentScoreId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Reputational Score Id Required")]
         public int ReputationalScoreId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Resource Profile Score Required")]
         public int ResourceProfileScoreId { get; set; }
         public int ProjectScore { get; set; }
         public bool CustomerFeedback { get; set; }
